773a99d85a7688cb956c8ca80aea6782611f9a9c
Dean Wette <wetted@objectcomputing.com>
2022-04-08 12:09:24 -0500

    add comment

--- src/test/groovy/com/example/ExampleTest.groovy
+++ src/test/groovy/com/example/ExampleTest.groovy
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.junit.jupiter.api.Test
 
+// this passes, original Spock spec fails
 @SpringBootTest
 class ExampleTest {
     
