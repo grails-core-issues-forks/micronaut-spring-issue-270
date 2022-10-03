1192a5e5357d801355ea73d4d35a2de98ed2ffa8
Dean Wette <wetted@objectcomputing.com>
2022-10-03 15:23:11 -0500

    upgrade dependencies to latest, including micronaut 3.7.1, micronaut-spring 4.3.1, spring boot 2.7.4

--- src/test/groovy/com/example/ExampleTest.groovy
+++ src/test/groovy/com/example/ExampleTest.groovy
@@ -2,17 +2,36 @@ package com.example
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.junit.jupiter.api.Test
 
 // this passes, original Spock spec fails
+//import org.junit.jupiter.api.Test
+//@SpringBootTest
+//class ExampleTest {
+//
+//    @Autowired
+//    MicronautService micronautService
+//
+//    @Test
+//    void testItWorks() {
+//        expect:
+//        micronautService.random()
+//    }
+//}
+
+
+// this fails with:
+//  Message: No bean of type [io.micronaut.core.cli.CommandLine] exists. Make sure the bean is not disabled by bean
+//  requirements (enable trace logging for 'io.micronaut.context.condition' to check) and if the bean is enabled then
+//  ensure the class is declared a bean and annotation processing is enabled (for Java and Kotlin the 'micronaut-inject-java'
+//  dependency should be configured as an annotation processor).
+import spock.lang.Specification
 @SpringBootTest
-class ExampleTest {
-    
+class ExampleTest extends Specification {
+
     @Autowired
     MicronautService micronautService
 
-    @Test
-    void testItWorks() {
+    void "context loads correctly"() {
         expect:
         micronautService.random()
     }
