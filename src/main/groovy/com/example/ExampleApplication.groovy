200be7b3fc71700be265c7884b828557c11bd608
Dean Wette <wetted@objectcomputing.com>
2023-03-27 16:52:36 -0500

    cleanup

--- src/main/groovy/com/example/ExampleApplication.groovy
+++ src/main/groovy/com/example/ExampleApplication.groovy
@@ -1,29 +1,13 @@
 package com.example
 
-import io.micronaut.core.cli.CommandLine
-import io.micronaut.spring.beans.MicronautBeanProcessor
-import jakarta.inject.Singleton
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
 class ExampleApplication {
 
     static void main(String[] args) {
         SpringApplication.run(ExampleApplication.class, args)
-
-//        SpringApplicationBuilder builder = new SpringApplicationBuilder();
-//        MicronautApplicationContext context = new MicronautApplicationContext();
-//        context.start();
-//        builder.parent(context);
-//        builder.sources(ExampleApplication);
-//        builder.build().run();
     }
-
-//    @Bean
-//    MicronautBeanProcessor micronautBeanProcessor() {
-//        return new MicronautBeanProcessor(Singleton.class, CommandLine.class)
-//    }
 }
